3d6e07c63d762625aabaf5dc7c5adac1bcb11401
Victor Souza <euosouza@gmail.com>
2024-03-09 16:23:06 -0300

    feat: create component text-section

---
+++ src/components/TextSectionPageComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-3xl font-bold">{{ title }}</h1>
+  <p>{{ text }}</p>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const { title, text } = defineProps({
+  title: { type: String, required: true},
+  text: { type: String, required: true},
+});
+
+</script>
