f457c56bbed308617ac53239d0a024ef79a17967
Victor Souza <euosouza@gmail.com>
2024-03-09 16:21:54 -0300

    feat: create component back-router-link

---
+++ src/components/BackRouterLinkComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <RouterLink :to="path" class="flex items-center gap-2 max-w-max mb-8 bg-transparent hover:text-brand duration-200">
+    <FiArrowLeft />
+    <span>{{ title }}</span>
+  </RouterLink>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+import {  FiArrowLeft } from "vue3-icons/fi";
+
+const { title, path } = defineProps({
+  title: { type: String, required: true},
+  path: { type: String, required: true},
+});
+
+</script>
+
