d949de05298298659930c6d2f8b0e42f69539606
Victor Souza <euosouza@gmail.com>
2024-03-09 16:22:07 -0300

    feat: create component button

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="flex items-center justify-center gap-2 bg-gray-900 h-12
+    hover:bg-gray-700 duration-200 ease-in
+    box-border p-4 fill-current text-white stroke-2 rounded w-48"
+    v-bind="$attrs"
+    @click="handleClick"
+    >
+    {{ title }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+const emit = defineEmits(["handle-click"]);
+
+const { title } = defineProps({
+  title: { type: String, required: true}
+});
+
+const handleClick = () => {
+  emit("handle-click");
+};
+</script>
