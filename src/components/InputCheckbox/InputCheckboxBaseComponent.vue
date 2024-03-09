88939d912e2e8872d906811a82287228d662dd19
Victor Souza <euosouza@gmail.com>
2024-03-09 16:23:39 -0300

    feat: create component input-checkbox

---
+++ src/components/InputCheckbox/InputCheckboxBaseComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <input
+    @input="updateValue"
+    type="checkbox"
+    :checked="modelValue"
+    :value="modelValue"
+    v-bind="$attrs"
+  >
+
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const emits = defineEmits(["update:modelValue"]);
+
+const { modelValue } = defineProps({
+  modelValue: {type: Boolean, required: true },
+});
+
+const updateValue = (e: Event) => {
+  if (e.target) emits("update:modelValue", (e.target as HTMLInputElement).checked);
+};
+</script>
---
+++ src/components/InputCheckbox/InputCheckboxWrapperComponent .vue
@@ -0,0 +1,19 @@
+<template>
+  <label :for="forName" class="flex items-center gap-2 cursor-pointer font-semibold text-sm">
+    <slot name="input" />
+    <span>
+      {{ label }}
+    </span>
+  </label>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const { label } = defineProps({
+  label: {type: String, required: true },
+  forName: {type: String, required: true },
+});
+
+
+</script>
