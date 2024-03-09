bab0eb1f1c495d9d9e47c808f7a7486847b4be95
Victor Souza <euosouza@gmail.com>
2024-03-09 16:23:24 -0300

    feat: create component input

---
+++ src/components/Input/InputBaseComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <input
+    class="w-full md:w-3/4 lg:w-2/4 p-4 pl-10 h-12
+      border-gray-400 border rounded box-borderplaceholder-gray-400"
+    @input="updateValue"
+    :value="modelValue"
+    v-bind="$attrs"
+  >
+
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const emits = defineEmits(["update:modelValue"]);
+
+const { modelValue } = defineProps({
+  modelValue: {type: String, required: true },
+});
+
+const updateValue = (e: Event) => {
+  if (e.target) emits("update:modelValue", (e.target as HTMLInputElement).value);
+};
+</script>
---
+++ src/components/Input/InputWrapperComponent .vue
@@ -0,0 +1,22 @@
+<template>
+  <label :for="forName" class="text-sm grid gap-1 cursor-pointer">
+    <span class="font-bold" v-if="label">
+      {{ label }}
+    </span>
+    <span class="relative flex items-center ">
+      <slot name="icon" />
+      <slot name="input" />
+    </span>
+  </label>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const { label } = defineProps({
+  label: {type: String, required: true },
+  forName: {type: String, required: true },
+});
+
+
+</script>
