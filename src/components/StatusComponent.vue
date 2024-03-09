127466b3fd27cd4ffe73dce7679c91ea971cec8d
Victor Souza <euosouza@gmail.com>
2024-03-09 16:22:50 -0300

    feat: create component status

---
+++ src/components/StatusComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex items-center gap-2">
+    <span class="block w-2 h-2 rounded-full" :class="color" ></span>
+    <span>{{ title }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const { title } = defineProps({
+  title: { type: String, required: true },
+  color: { type: String, required: true }
+});
+
+</script>
