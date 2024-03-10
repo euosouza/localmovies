325711ed9a3f696d5705c6367d1d1bc4b8a34712
Victor Souza <euosouza@gmail.com>
2024-03-10 14:24:18 -0300

    feat: create input select component

---
+++ src/components/InputSelect/InputSelectBaseComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <select
+    class="p-4 pl-10 h-12 border-gray-500 border rounded box-border placeholder-gray-400 w-full"
+    v-bind="$attrs"
+    :value="modelValue"
+    @change="updateValue"
+  >
+    <option value="" selected disabled class="">Status</option>
+    <option :value="item" v-for="(item, index) in list" :key="index">{{ item }}</option>
+  </select>
+
+</template>
+
+<script lang="ts" setup>
+import { defineProps, PropType } from "vue";
+
+const emits = defineEmits(["update:modelValue"]);
+
+const { modelValue, list } = defineProps({
+  modelValue: {type: String, required: true },
+  list: {type: Array as PropType<string[]>, required: true }
+});
+
+const updateValue = (e: Event) => {
+  if (e.target) emits("update:modelValue", (e.target as HTMLInputElement).value);
+};
+</script>
---
+++ src/components/InputSelect/InputSelectWrapperComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <label :for="forName" class="text-sm grid gap-1 cursor-pointer">
+    <span class="font-bold" v-if="label">
+      {{ label }}
+    </span>
+    <span class="relative flex items-center ">
+      <span class="absolute left-3 text-lg"><slot name="icon" /></span>
+
+      <slot name="input" />
+    </span>
+  </label>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const { label } = defineProps({
+  label: {type: String  },
+  forName: {type: String, required: true },
+});
+
+
+</script>
